84d0382e739a521710fe6984990bade07e61cc7a
Angela Remolina <angela.remolina.2019@upb.edu.co>
2024-12-12 16:55:21 +0100

    add Attention u-net arquitecture

--- Model.py
+++ Model.py
@@ -1,6 +1,8 @@
 import keras
 
 NUM_CLASSES = 5
+INPUT_SHAPE = (64, 128, 1)
+
 
 def configure(num_classes=None):
     """
@@ -11,22 +13,20 @@ def configure(num_classes=None):
         NUM_CLASSES = num_classes
 
 
-
 def u_net(
-          input_shape=(64,128,1), 
-          depth=4, 
-          d_conv_count = 3, 
-          b_conv_count = 3, 
-          u_conv_count = 3, 
-          start_filter=32, 
-          skip_connections=True, 
-          dropout=0.2
-          ):
-    
+    input_shape=INPUT_SHAPE,
+    depth=4,
+    d_conv_count=3,
+    b_conv_count=3,
+    u_conv_count=3,
+    start_filter=32,
+    skip_connections=True,
+    dropout=0.2,
+):
     """
     Parametrizable definition of a UNet model, things to be tried and tuned are
     - Deeper network
-    - Remove batch normalisation in upsampling path
+    - Remove batch normalization in upsampling path
     - Conv2DTranspose instead of bilinear interpolation for upsampling
     """
 
@@ -34,9 +34,11 @@ def u_net(
     print("FUNCTION U-NET")
     print("Building the model architecture...")
     # Input Layer
-    inputs = keras.layers.Input(shape=input_shape, name='input_layer')
+    inputs = keras.layers.Input(shape=input_shape, name="input_layer")
 
-    normalized_inputs = keras.layers.Rescaling(1.0 / 255.0)(inputs)  # Normalize to [0, 1]
+    normalized_inputs = keras.layers.Rescaling(1.0 / 255.0)(
+        inputs
+    )  # Normalize to [0, 1]
 
     x = normalized_inputs  # Pass normalized inputs to the rest of the model
 
@@ -44,36 +46,34 @@ def u_net(
     # Downsampling
     for i in range(depth):
         for j in range(d_conv_count):
-            x = keras.layers.Conv2D(filters=start_filter,
-                            kernel_size=(3,3),
-                            strides=(1, 1),
-                            padding='same')(x)
-            x = keras.layers.BatchNormalization()(x) 
+            x = keras.layers.Conv2D(
+                filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+            )(x)
+            x = keras.layers.BatchNormalization()(x)
             x = keras.layers.ReLU()(x)
 
         if skip_connections:
             # Save the layer for skip connections
             skipped.append(x)
 
-        x = keras.layers.MaxPooling2D(pool_size=(2,2))(x)
+        x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
         x = keras.layers.Dropout(dropout)(x)
-        
+
         start_filter = start_filter * 2
-    
+
     # Bottleneck
     for j in range(b_conv_count):
-        x = keras.layers.Conv2D(filters=start_filter,
-                            kernel_size=(3,3),
-                            strides=(1, 1),
-                            padding='same')(x)
-        x = keras.layers.BatchNormalization()(x) 
+        x = keras.layers.Conv2D(
+            filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+        )(x)
+        x = keras.layers.BatchNormalization()(x)
         x = keras.layers.ReLU()(x)
-    
+
     start_filter = start_filter // 2
 
     # Upsampling
     for i in range(depth):
-        x = keras.layers.UpSampling2D(2, interpolation='bilinear')(x)
+        x = keras.layers.UpSampling2D(2, interpolation="bilinear")(x)
 
         if skip_connections:
             x = keras.layers.Concatenate()([x, skipped[depth - i - 1]])
@@ -81,24 +81,137 @@ def u_net(
         x = keras.layers.Dropout(dropout)(x)
 
         for j in range(u_conv_count):
-            x = keras.layers.Conv2D(filters=start_filter,
-                            kernel_size=(3,3),
-                            strides=(1, 1),
-                            padding='same')(x)
-            x = keras.layers.BatchNormalization()(x) ## remove?
+            x = keras.layers.Conv2D(
+                filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+            )(x)
+            x = keras.layers.BatchNormalization()(x)  ## remove?
             x = keras.layers.ReLU()(x)
         start_filter = start_filter // 2
-    
 
     # Output Layer
-    outputs = keras.layers.Conv2D(filters=NUM_CLASSES,
-                        kernel_size=(1,1),
-                        strides=(1, 1),
-                        padding='same',
-                        activation='softmax',
-                        name="output_layer")(x)
-    
-    model = keras.Model(inputs, outputs, name='UNet')
+    outputs = keras.layers.Conv2D(
+        filters=NUM_CLASSES,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding="same",
+        activation="softmax",
+        name="output_layer",
+    )(x)
+
+    model = keras.Model(inputs, outputs, name="UNet")
+
+    print("Completed model architecture")
+    print()
+
+    return model
+
+
+def attention_u_net(
+    input_shape=INPUT_SHAPE,
+    depth=4,
+    d_conv_count=3,
+    b_conv_count=3,
+    u_conv_count=3,
+    start_filter=32,
+    skip_connections=True,
+    dropout=0.2,
+    NUM_CLASSES=NUM_CLASSES,
+):
+    """
+    Parametrizable definition of UNet model with attention, things to be tried and tuned are
+    - Deeper network
+    - Remove batch normalization in upsampling path
+    - Add a different normalization layer
+    - Conv2DTranspose instead of bilinear interpolation for upsampling
+    """
+
+    print()
+    print("FUNCTION ATTENTION U-NET")
+    print("Building the model architecture...")
+    # Input Layer
+    inputs = keras.layers.Input(shape=input_shape, name="input_layer")
+
+    normalized_inputs = keras.layers.Rescaling(1.0 / 255.0)(
+        inputs
+    )  # Normalize to [0, 1]
+
+    x = normalized_inputs  # Pass normalized inputs to the rest of the model
+
+    skipped = []
+
+    # Downsampling
+    for i in range(depth):
+
+        for j in range(d_conv_count):
+            x = keras.layers.Conv2D(
+                filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+            )(x)
+            x = keras.layers.BatchNormalization()(x)
+            x = keras.layers.ReLU()(x)
+
+        if skip_connections:
+            # Save the layer for skip connections
+            skipped.append(x)
+
+        x = keras.layers.MaxPooling2D(pool_size=(2, 2))(x)
+        x = keras.layers.Dropout(dropout)(x)
+
+        start_filter = start_filter * 2
+
+    # Bottleneck
+    for j in range(b_conv_count):
+        x = keras.layers.Conv2D(
+            filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+        )(x)
+        x = keras.layers.BatchNormalization()(x)
+        x = keras.layers.ReLU()(x)
+
+    start_filter = start_filter // 2
+
+    # Upsampling
+    for i in range(depth):
+
+        x = keras.layers.UpSampling2D(2, interpolation="bilinear")(x)
+
+        if skip_connections:
+            # Apply attention mechanism to the skip connections
+            g = skipped[depth - i - 1]
+            inter_channel = start_filter
+            # Generate query, key, and value tensors
+            theta_x = keras.layers.Conv2D(inter_channel, (1, 1), padding="same")(
+                x
+            )  # Query
+            phi_g = keras.layers.Conv2D(inter_channel, (1, 1), padding="same")(g)  # Key
+            attn = keras.layers.Activation("sigmoid")(
+                keras.layers.Add()([theta_x, phi_g])
+            )  # Attention Map
+            attn = keras.layers.Conv2D(1, (1, 1), padding="same")(attn)  # Value
+
+            # Multiply attention map with the input feature map
+            x = keras.layers.Multiply()([x, attn])
+
+        x = keras.layers.Dropout(dropout)(x)
+
+        for j in range(u_conv_count):
+            x = keras.layers.Conv2D(
+                filters=start_filter, kernel_size=(3, 3), strides=(1, 1), padding="same"
+            )(x)
+            x = keras.layers.BatchNormalization()(x)
+            x = keras.layers.ReLU()(x)
+
+        start_filter = start_filter // 2
+
+    # Output Layer
+    outputs = keras.layers.Conv2D(
+        filters=NUM_CLASSES,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding="same",
+        activation="softmax",
+        name="output_layer",
+    )(x)
+
+    model = keras.Model(inputs, outputs, name="Attention_UNet")
 
     print("Completed model architecture")
     print()
