3c613913452c193ec8589413fb873837b4c62095
Pınar Erbil <63547373+erbilpinar@users.noreply.github.com>
2024-12-13 17:34:39 +0100

    Dense UNet Implementation

--- Model.py
+++ Model.py
@@ -223,3 +223,101 @@ def attention_u_net(
 
     model = keras.Model(inputs, outputs, name="Attention_UNet")
     return model
+
+
+#############################
+# DENSE UNET                #
+#############################
+
+def dense_u_net(
+          input_shape=(64,128,1), 
+          depth=4,
+          d_conv_count = 3, 
+          b_conv_count = 3, 
+          u_conv_count = 3, 
+          start_filter=32, 
+          dropout=0.2
+          ):
+    
+    print()
+    print("FUNCTION DENSE U-NET")
+    print("Building the model architecture...")
+    # Input Layer
+    inputs = keras.layers.Input(shape=input_shape, name='input_layer')
+
+    normalized_inputs = keras.layers.Rescaling(1.0 / 255.0)(inputs)  # Normalize to [0, 1]
+
+    next = normalized_inputs  # Pass normalized inputs to the rest of the model
+
+    skipped = []
+    growth_rate = 16
+    # Downsampling
+    for i in range(depth):
+        for j in range(d_conv_count): # Dense Block
+            x = keras.layers.Conv2D(filters=start_filter,
+                            kernel_size=(3,3),
+                            strides=(1, 1),
+                            padding='same',
+                            kernel_initializer="he_normal")(next)
+            
+            x = keras.layers.BatchNormalization()(x) 
+            conv_block = keras.layers.ReLU()(x)
+            
+            next = keras.layers.Concatenate()([conv_block, next])
+
+        skipped.append(conv_block)
+
+        x = keras.layers.MaxPooling2D(pool_size=(2,2))(conv_block)
+        next = keras.layers.Dropout(dropout)(x)
+        
+        start_filter = start_filter * 2
+    
+    # Bottleneck
+    for j in range(b_conv_count):
+        x = keras.layers.Conv2D(filters=start_filter,
+                            kernel_size=(3,3),
+                            strides=(1, 1),
+                            kernel_initializer="he_normal",
+                            padding='same')(next)
+        x = keras.layers.BatchNormalization()(x) 
+        x = keras.layers.ReLU()(x)
+        next = keras.layers.Dropout(dropout)(x)
+
+    start_filter = start_filter // 2
+
+    # Upsampling
+    for i in range(depth):
+        x = keras.layers.Conv2DTranspose(
+                filters=start_filter,
+                kernel_size=(3, 3),
+                strides=(2, 2),
+                padding="same",
+                )(next)
+        x = keras.layers.Concatenate()([x, skipped.pop()])
+        next = keras.layers.Dropout(dropout)(x)
+
+        for j in range(u_conv_count): # Dense Block
+            x = keras.layers.Conv2D(filters=start_filter,
+                            kernel_size=(3,3),
+                            strides=(1, 1),
+                            padding='same',
+                            kernel_initializer="he_normal")(next)            
+            x = keras.layers.BatchNormalization()(x)
+            conv_block = keras.layers.ReLU()(x)
+            next =  keras.layers.Concatenate()([conv_block, next])
+        start_filter = start_filter // 2
+    
+    # Output Layer
+    outputs = keras.layers.Conv2D(filters=NUM_CLASSES,
+                        kernel_size=(1,1),
+                        strides=(1, 1),
+                        padding='same',
+                        activation='softmax',
+                        name="output_layer")(next)
+    
+    model = keras.Model(inputs, outputs, name='Dense_UNet')
+
+    print("Completed model architecture")
+    print()
+
+    return model
